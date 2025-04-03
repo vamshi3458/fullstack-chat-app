83f68d44f24ea85afc643572fb7a97348e7f45b7
vamshi3458 <129094744+vamshi3458@users.noreply.github.com>
2025-04-03 18:37:02 +0530

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -61,7 +61,7 @@ const SignUpPage = () => {
               <input
               type="text"
               className={`input input-bordered w-full pl-10`}
-              placeholder="Ex. Jaipal Reddy Yarram"
+              placeholder="Ex. Vamshi"
               value={formData.fullName}
               onChange={(e)=>setFormData({ ...formData, fullName: e.target.value })}
               />
